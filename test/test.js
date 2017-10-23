32542b89a85b5bf1fa62680e8deb16c604c651de
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-10-23 19:02:09 +0200

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var Hacktoberfest = require('../main.js')
+
+it('Check if Hacktoberfeststats work', function(done) {
+Hacktoberfest("MatejMecka", "2017", function(hacktoberfestStats,error){
+        expect(hacktoberfestStats.mainStats.Name).to.equal('MatejMecka')
+   })
+});
