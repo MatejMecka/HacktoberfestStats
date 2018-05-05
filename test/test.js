d60ce954bb30c6ac278513bf2b52696d447d50cf
Matej Plavevski <matej.plavevski+github@gmail.com>
2018-05-05 21:30:30 +0200

    GitHub and their Changes....

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var Hacktoberfest = require('../main.js')
 
 it('Check if Hacktoberfeststats work', function(done) {
   Hacktoberfest('MatejMecka', '2017', function(hacktoberfestStats, error) {
-    expect(hacktoberfestStats.mainStats.Name).to.equal('MatejMecka')
+    expect(hacktoberfestStats.mainStats.Name).to.equal('Matej Plavevski')
     done()
   })
 })
