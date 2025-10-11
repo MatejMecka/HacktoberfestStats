0756652222697141e7fdd31f35478448b9311d3a
Matej Plavevski <matej.plavevski+github@gmail.com>
2025-10-11 17:19:38 +0200

    Add missing packages for tests

--- built/example.js
+++
@@ -1,7 +0,0 @@
-import { getHacktoberfestStats } from './main';
-getHacktoberfestStats('MatejMecka', 2017, function (hacktoberfestStats) {
-    console.log(hacktoberfestStats);
-});
-getHacktoberfestStats('MatejMecka', function (hacktoberfestStats) {
-    console.log(hacktoberfestStats);
-});
