9c84d7a063251d14421ade6e92b4df2602814aac
Matej Plavevski <matej.plavevski+github@gmail.com>
2025-10-12 17:33:10 +0200

    Add tests with await

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,20 +2,30 @@ import { expect } from 'chai';
 import { getHacktoberfestStats } from '../built/main.js';
 
 it('Check if Hacktoberfeststats (callback) work', function (done) {
-  getHacktoberfestStats('MatejMecka', '2017', (stats) => {
+  getHacktoberfestStats('MatejMecka', 2017, (stats) => {
     expect(stats.current).to.equal(14);
     done();
   });
 });
 
 it('Check if Hacktoberfeststats (promise) work', function (done) {
-  getHacktoberfestStats('MatejMecka', '2017').then((stats) => {
+  getHacktoberfestStats('MatejMecka', 2017).then((stats) => {
     expect(stats.current).to.equal(14);
     done();
   });
 });
 
 it('Check if Hacktoberfeststats throws an error', function (done) {
-  expect(getHacktoberfestStats.bind(getHacktoberfestStats, 'MatejMecka', '3000')).to.throw();
+  expect(getHacktoberfestStats.bind(getHacktoberfestStats, 'MatejMecka', 3000)).to.throw();
   done();
 });
+
+it('Check if Hacktoberfeststats (using await) work', async function () {
+  const stats = await getHacktoberfestStats('MatejMecka', 2017);
+  expect(stats.current).to.equal(14);
+});
+
+it('Check if Hacktoberfeststats (using await) work throws an error', async function () {
+  expect(await getHacktoberfestStats.bind(getHacktoberfestStats, 'MatejMecka', 3000)).to.throw();
+});
+
